4636009262e123692203cae59139df5911e13cc0
RyougiShiki <yzm805482013@gmail.com>
2024-05-05 22:12:47 +1000

    list participants successfully

--- client/android/lib/screens/room_screen.dart
+++ client/android/lib/screens/room_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:android/models/room.dart'; // 确保 Room 模型已正确定义
+import 'package:android/models/user.dart'; // 确保 User 模型已正确定义
 
 class RoomScreen extends StatefulWidget {
   final Room room;
@@ -11,8 +15,50 @@ class RoomScreen extends StatefulWidget {
 }
 
 class _RoomScreenState extends State<RoomScreen> {
-  Color selectedColor = Colors.black; // 默认颜色
-  String selectedTool = 'pen'; // 默认工具
+  List<User> participants = [];
+  Color selectedColor = Colors.black;
+  String selectedTool = 'pen';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchParticipants();
+  }
+
+  void fetchParticipants() async {
+    var serverUrl = dotenv.env['SERVER_URL'] ?? "http://defaultserver";
+    var url =
+        Uri.parse('$serverUrl/rooms/getparticipants?roomId=${widget.room.id}');
+
+    var authUser = dotenv.env['USER'] ?? "admin";
+    var authPwd = dotenv.env['PASSWORD'] ?? "admin";
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$authUser:$authPwd'));
+
+    try {
+      var response = await http.get(
+        url,
+        headers: <String, String>{'authorization': basicAuth},
+      );
+      if (response.statusCode == 200) {
+        setState(() {
+          participants = (jsonDecode(response.body) as List)
+              .map((data) => User.fromJson(data))
+              .toList();
+        });
+      } else {
+        print(
+            'Failed to load participants with status code: ${response.statusCode}');
+        print('Response body: ${response.body}');
+        throw Exception(
+            'Failed to load participants with status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to load participants: $e')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,20 +116,26 @@ class _RoomScreenState extends State<RoomScreen> {
     );
   }
 
-  void _showParticipants(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return ListView.builder(
-          itemCount: widget.room.participants.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(widget.room.participants[index].username),
-            );
-          },
-        );
-      },
-    );
+  void _showParticipants(BuildContext context) async {
+    try {
+      fetchParticipants(); // Wait for participants to be fetched
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return ListView.builder(
+            itemCount: participants.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(participants[index].username),
+              );
+            },
+          );
+        },
+      );
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error fetching participants: $error")));
+    }
   }
 
   void _leaveRoom(BuildContext context) {
