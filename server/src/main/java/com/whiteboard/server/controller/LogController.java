120d8a6029689044e0821e7304768eab11f29eee
RyougiShiki <yzm805482013@gmail.com>
2024-05-16 23:16:31 +1000

    fix error

--- server/src/main/java/com/whiteboard/server/controller/LogController.java
+++ server/src/main/java/com/whiteboard/server/controller/LogController.java
@@ -2,21 +2,22 @@ package com.whiteboard.server.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import java.util.Map;
 
 @Controller
 public class LogController {
     private static final Logger logger = LoggerFactory.getLogger(LogController.class);
 
     @SubscribeMapping("/welcome")
-    public String sendWelcomeMessage(@Headers Map<String, Object> headers) {
-        logger.info("Client connected: " + StompHeaderAccessor.wrap(headers).getSessionId());
+    public String sendWelcomeMessage(Message<?> message) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+        logger.info("Client connected: " + headerAccessor.getSessionId());
         return "Welcome to the WebSocket server!";
     }
 }
